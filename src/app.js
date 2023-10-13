7297979659c0ad2957c80a17d19b7b5139179525
a1996k <krishna.ab@northeastern.edu>
2023-10-12 22:41:41 -0400

    Update after review comments

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser'); 
 
 // const sequelize = require('./config/database');
-const { synchronizeDatabase } = require('./config/database');
+// const { synchronizeDatabase } = require('./config/database');
 const validateRequestMethod = require('./middleware/validateRequestMethod');
 const validatePayload = require('./middleware/validatePayload');
 const checkRequestParameters = require('./middleware/checkRequestParameters');
@@ -13,11 +13,9 @@ const assignmentRoute = require('./routes/assignmentRoute');
 
 require("dotenv").config();
 
-const port = process.env.PORT || 3000;
-console.log("in app 17");
 // Use the middlewares
 app.use(validateRequestMethod, checkRequestParameters, validatePayload);
-console.log("in app 20");
+
 // Mount the healthz router
 app.use('/healthz', healthzRouter);
 // Mount the authenticate router
@@ -26,15 +24,7 @@ app.use('/authenticate', authenticateRouter);
 app.use(bodyParser.json());
 // Mount the assignmentRouter
 app.use('/v1/assignments', assignmentRoute);
-// synchronizeDatabase().then(() => {
-//     console.log('-----RUN synchronizeDatabase()-------');
-//     app.listen(port, () => {
-//         console.log(`Example app listening on port ${port}`);
-//     });
-// })
-// .catch((error) =>{
-//     console.error('Error synchronizing database:', error);
-// });
+
 
 module.exports = app;
 
