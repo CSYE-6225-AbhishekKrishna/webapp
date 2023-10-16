fb64ed6bb812aa7353b93018586a3993ec85865a
a1996k <krishna.ab@northeastern.edu>
2023-10-16 16:30:19 -0400

    updated the build.sh file as it failed in the runner

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,26 @@
 
 ls
 
+# Update the package list to ensure we can install packages
+sudo apt-get update
+
+# Install 'unzip' package
 sudo apt-get install unzip -y
-echo "===============================installed unzip==============================="
+echo "=============================== Installed unzip ==============================="
 
-sudo apt-get install nodejs npm -y 
-echo "===============================installed Node and NPM==============================="
+# Install 'nodejs' and 'npm' packages
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+echo "=============================== Installed Node and NPM ==============================="
 
-sudo apt-get install postgresql postgresql-contrib -y 
-echo "===============================installed unzip==============================="
+# Install 'postgresql' and 'postgresql-contrib' packages
+sudo apt-get install postgresql -y
+sudo apt-get install postgresql-contrib -y
+echo "=============================== Installed PostgreSQL and Contrib ==============================="
 
+# Change the password for the 'postgres' user
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'Pa55w0rd';"
-echo "===============================Passw0rd for user postgres changes==============================="
+echo "=============================== Password for user 'postgres' changed ==============================="
 
+# Print the current working directory
 pwd
