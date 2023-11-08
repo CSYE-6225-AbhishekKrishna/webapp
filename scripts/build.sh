351b61ae3e80fd4e3b85c5658e6969537487f931
a1996k <krishna.ab@northeastern.edu>
2023-11-08 14:10:14 -0500

    updating error in script file

--- scripts/build.sh
+++ scripts/build.sh
@@ -94,11 +94,11 @@ sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
 
 # Creating file for logging
-sudo touch /var/log/info-log-csye6225.log
+sudo touch /var/logs/info-log-csye6225.log
 
 # Modefying Permissions
 sudo chown -R csye6225:csye6225 /opt
-sudo chown csye6225:csye6225 /var/log/info-log-csye6225.log
+sudo chown csye6225:csye6225 /var/logs/info-log-csye6225.log
 
 # Enable and start the service
 echo "STARTING MYWEBAPP"
